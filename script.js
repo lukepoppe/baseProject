75bf0911ce960d440829c2e9357d19de4c486b7e
lukepoppe <lukepoppe@hotmail.com>
2015-04-30 13:26:34 -0500

    new

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+function searchCallback(results) {
+
+
+
+}
+
+
+
+
+$(document).ready(function(){
+	$('.btn').on('click', function(){
+		$('#searchResults').empty();
+		//userInput = $('search').val();
+		search("batman");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function search(query){
+
+	$.ajax ({
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    crossDomain: true,
+	    jsonp: 'json_callback',
+	    url: 'http://www.giantbomb.com/api/search/?format=jsonp&resources=game&api_key=' + apikey +'&query=' + encodeURI(query),
+	    complete: function() {
+	        console.log('ajax complete');
+	    },
+	    success: function(data) {
+	        searchCallback(data.results);
+	    }
+	});
+
+}
